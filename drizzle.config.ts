897ac517d86841cfbb481e7b7bd7d3f729bf9751
The-FerryMan1 <mercedroxy@gmail.com>
2025-11-08 12:26:45 +0800

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './src/database/migration',
+  schema: [
+    './src/database/schema.ts',
+    './auth-schema.ts'
+  ],
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL as string,
+  },
+});
