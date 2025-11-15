af3fbbb0f2792b0a473d4ddb998966b1b1acc653
The-FerryMan1 <mercedroxy@gmail.com>
2025-11-15 09:42:30 +0800

    new migration, added transaction revert

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,53 +1,75 @@
-import { doublePrecision, integer, pgEnum, pgTable, text, timestamp, varchar,boolean } from "drizzle-orm/pg-core";
-import { user } from '../../auth-schema'
-
-
+import {
+  doublePrecision,
+  integer,
+  pgEnum,
+  pgTable,
+  text,
+  timestamp,
+  varchar,
+  boolean,
+} from "drizzle-orm/pg-core";
+import { user } from "../../auth-schema";
 
 export enum CategoryType {
-    INCOME= 'income',
-    EXPENSE = 'expense'
+  INCOME = "income",
+  EXPENSE = "expense",
 }
 
 export enum TransactionType {
-    ORIGINAL= 'original',
-    MODIFIED = 'modified'
+  ORIGINAL = "original",
+  MODIFIED = "modified",
+  REVERT = "revert",
 }
 
 export const TransactionEnum = pgEnum("transaction_type", [
-    TransactionType.ORIGINAL,
-    TransactionType.MODIFIED
-] as const)
+  TransactionType.ORIGINAL,
+  TransactionType.MODIFIED,
+  TransactionType.REVERT,
+] as const);
 
 export const categoryEnum = pgEnum("category_type", [
-    CategoryType.EXPENSE,
-    CategoryType.INCOME
-] as const)
+  CategoryType.EXPENSE,
+  CategoryType.INCOME,
+] as const);
 
-export const balance = pgTable('balance', {
-    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-    userID: text('user_id').notNull().references(() => user.id, { onDelete: "cascade" }),
-    createdAT: timestamp("created_at").defaultNow(),
-    currentBalance: doublePrecision('current_balance').notNull().default(0),
-    trash: boolean('trash').notNull().default(false),
-    balanceType: varchar('balance_type', {length:255})
-})
+export const balance = pgTable("balance", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  userID: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  createdAT: timestamp("created_at").defaultNow(),
+  currentBalance: doublePrecision("current_balance").notNull().default(0),
+  trash: boolean("trash").notNull().default(false),
+  balanceType: varchar("balance_type", { length: 255 }),
+});
 
-export const category = pgTable('category', {
-    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-    userID: text('user_id').notNull().references(() => user.id, { onDelete: "cascade" }),
-    name: varchar('name', { length: 255 }).notNull().default("unamaed"),
-    trash: boolean('trash').notNull().default(false),
-    type: categoryEnum().notNull().default(CategoryType.EXPENSE)
-})
+export const category = pgTable("category", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  userID: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  name: varchar("name", { length: 255 }).notNull().default("unamaed"),
+  trash: boolean("trash").notNull().default(false),
+  type: categoryEnum().notNull().default(CategoryType.EXPENSE),
+});
 
-export const transaction = pgTable('transaction', {
-    id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-    userID: text('user_id').notNull().references(() => user.id, { onDelete: "cascade" }),
-    balanceID: integer('balance_id').notNull().references(() => balance.id, { onDelete: 'cascade' }),
-    categoryID: integer('category_id').notNull().references(() => category.id, { onDelete: 'cascade' }),
-    amount: doublePrecision('amount').notNull(),
-    description: varchar("description", { length: 255 }).default("no description"),
-    type:TransactionEnum().notNull().default(TransactionType.ORIGINAL),
-    trash: boolean('trash').notNull().default(false),
-    date: timestamp("date").defaultNow(),
-})
+export const transaction = pgTable("transaction", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  userID: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  balanceID: integer("balance_id")
+    .notNull()
+    .references(() => balance.id, { onDelete: "cascade" }),
+  categoryID: integer("category_id")
+    .notNull()
+    .references(() => category.id, { onDelete: "cascade" }),
+  amount: doublePrecision("amount").notNull(),
+  description: varchar("description", { length: 255 }).default(
+    "no description",
+  ),
+  type: TransactionEnum().notNull().default(TransactionType.ORIGINAL),
+  trash: boolean("trash").notNull().default(false),
+  date: timestamp("date").defaultNow(),
+});
+
--- src/modules/transaction/service.ts
+++ src/modules/transaction/service.ts
@@ -1,174 +1,185 @@
 import { and, eq, sql } from "drizzle-orm";
 import { db } from "../../database";
-import { balance, category, transaction } from "../../database/schema";
+import {
+  balance,
+  category,
+  transaction,
+  TransactionType,
+} from "../../database/schema";
 import { TransactionModel } from "./model";
 import { status } from "elysia";
 
-export async function createTransaction({ id: category_id }: TransactionModel.transactionParams, { amount, balance_id, description }: TransactionModel.transactionBody, user_id: string) {
+export async function createTransaction(
+  { id: category_id }: TransactionModel.transactionParams,
+  { amount, balance_id, description }: TransactionModel.transactionBody,
+  user_id: string,
+) {
+  const row = await db.transaction(async (tx) => {
+    const [row] = await tx
+      .insert(transaction)
+      .values({
+        amount,
+        categoryID: category_id,
+        balanceID: balance_id,
+        description,
+        userID: user_id,
+        date: new Date(Date.now()),
+      })
+      .returning({
+        id: transaction.id,
+        category_id: transaction.categoryID,
+        balance_id: transaction.balanceID,
+        amount: transaction.amount,
+        description: transaction.description,
+        date: transaction.date,
+      });
 
-    const row = await db.transaction(async (tx) => {
-        const [row] = await tx
-            .insert(transaction)
-            .values(
-                {
-                    amount,
-                    categoryID: category_id,
-                    balanceID: balance_id,
-                    description,
-                    userID: user_id,
-                    date: new Date(Date.now())
-                }
-            )
-            .returning(
-                {
-                    id: transaction.id,
-                    category_id: transaction.categoryID,
-                    balance_id: transaction.balanceID,
-                    amount: transaction.amount,
-                    description: transaction.description,
-                    date: transaction.date
+    if (!row)
+      throw status(
+        400,
+        "Unable to create new transaction" satisfies TransactionModel.transactionBodyInvalid,
+      );
 
-                }
-            )
+    const [currentBalance] = await tx
+      .select({ balance: balance.currentBalance })
+      .from(balance)
+      .where(and(eq(balance.id, balance_id), eq(balance.userID, user_id)));
 
-        if (!row) throw status(400, "Unable to create new transaction" satisfies TransactionModel.transactionBodyInvalid)
+    if (!currentBalance)
+      throw status(404, "Unable to create new transaction, balance not found");
 
+    if (currentBalance.balance < amount) {
+      throw status(403, "insufficient balance");
+    }
 
-        const [currentBalance] = await tx
-            .select({ balance: balance.currentBalance })
-            .from(balance)
-            .where(
-                and(
-                    eq(balance.id, balance_id),
-                    eq(balance.userID, user_id)
-                )
-            )
+    await tx
+      .update(balance)
+      .set({ currentBalance: sql`${balance.currentBalance} - ${amount}` })
+      .where(and(eq(balance.id, balance_id), eq(balance.userID, user_id)));
 
-        if (!currentBalance) throw status(404, "Unable to create new transaction, balance not found")
+    return row;
+  });
 
-        if (currentBalance.balance < amount) {
-            throw status(403, 'insufficient balance')
-        }
-
-        await tx
-            .update(balance)
-            .set({ currentBalance: sql`${balance.currentBalance} - ${amount}` })
-            .where(
-                and(
-                    eq(balance.id, balance_id),
-                    eq(balance.userID, user_id)
-                )
-            )
-
-        return row
-
-    })
-
-
-
-    return row
+  return row;
 }
 
-export async function readTrasaction({ id: category_id }: TransactionModel.transactionParams, user_id: string) {
+export async function readTrasaction(
+  { id: category_id }: TransactionModel.transactionParams,
+  user_id: string,
+) {
+  const checkCategoryRow = await db.$count(
+    category,
+    and(eq(category.id, category_id), eq(category.userID, user_id)),
+  );
 
+  if (!checkCategoryRow) throw status(404, "Category does not exists");
 
+  const row = await db
+    .select({
+      id: transaction.id,
+      category_id: transaction.categoryID,
+      balance_id: transaction.balanceID,
+      amount: transaction.amount,
+      description: transaction.description,
+      date: transaction.date,
+    })
+    .from(transaction)
+    .where(
+      and(
+        eq(transaction.categoryID, category_id),
+        eq(transaction.userID, user_id),
+      ),
+    );
 
-    const checkCategoryRow = await db.$count(category, and(
-        eq(category.id, category_id),
-        eq(category.userID, user_id)
-    ))
-
-    if (!checkCategoryRow) throw status(404, "Category does not exists")
-
-    const row = await db
-        .select(
-            {
-                id: transaction.id,
-                category_id: transaction.categoryID,
-                balance_id: transaction.balanceID,
-                amount: transaction.amount,
-                description: transaction.description,
-                date: transaction.date
-            }
-        )
-        .from(transaction)
-        .where(
-            and(
-                eq(transaction.categoryID, category_id),
-                eq(transaction.userID, user_id)
-            )
-        )
-
-    return row
+  return row;
 }
 
 export async function readTrasanctionHistory(userID: string) {
+  const row = await db
+    .select({
+      id: transaction.id,
+      category_id: transaction.categoryID,
+      balance_id: transaction.balanceID,
+      amount: transaction.amount,
+      description: transaction.description,
+      date: transaction.date,
+    })
+    .from(transaction)
+    .where(eq(transaction.userID, userID));
 
-
-    const row = await db
-        .select(
-            {
-                id: transaction.id,
-                category_id: transaction.categoryID,
-                balance_id: transaction.balanceID,
-                amount: transaction.amount,
-                description: transaction.description,
-                date: transaction.date
-            }
-        )
-        .from(transaction)
-        .where(
-            eq(transaction.userID, userID)
-        )
-        
-
-    return row
+  return row;
 }
 
-export async function softDeleteTransanction({id:transactionID}:TransactionModel.transactionParams, userID:string){
-    const rowCheck = await db
-    .$count(transaction, and(
-        eq(transaction.id, transactionID),
-        eq(transaction.userID, userID)
-    ))
+export async function softDeleteTransanction(
+  { id: transactionID }: TransactionModel.transactionParams,
+  userID: string,
+) {
+  const rowCheck = await db.$count(
+    transaction,
+    and(eq(transaction.id, transactionID), eq(transaction.userID, userID)),
+  );
 
-    if(!rowCheck) throw status(404, 'Not Found')
-        
-    await db
+  if (!rowCheck) throw status(404, "Not Found");
+
+  await db
     .update(transaction)
-    .set(
-        {
-            trash:true           
-        }
-    )
+    .set({
+      trash: true,
+    })
     .where(
-        and(
-            eq(transaction.id, transactionID),
-            eq(transaction.userID, userID)
-        )
-    )
+      and(eq(transaction.id, transactionID), eq(transaction.userID, userID)),
+    );
 
-    return
+  return;
 }
 
-export async function HardDeleteTransanction({id:transactionID}:TransactionModel.transactionParams, userID:string){
-    const rowCheck = await db
-    .$count(transaction, and(
-        eq(transaction.id, transactionID),
-        eq(transaction.userID, userID)
-    ))
+export async function HardDeleteTransanction(
+  { id: transactionID }: TransactionModel.transactionParams,
+  userID: string,
+) {
+  const rowCheck = await db.$count(
+    transaction,
+    and(eq(transaction.id, transactionID), eq(transaction.userID, userID)),
+  );
 
-    if(!rowCheck) throw status(404, 'Not Found')
-        
-    await db
+  if (!rowCheck) throw status(404, "Not Found");
+
+  await db
     .delete(transaction)
     .where(
-        and(
-            eq(transaction.id, transactionID),
-            eq(transaction.userID, userID)
-        )
-    )
+      and(eq(transaction.id, transactionID), eq(transaction.userID, userID)),
+    );
 
-    return
-}   
+  return;
+}
 
+export async function revertTransaction(
+  { id: transactionID }: TransactionModel.transactionParams,
+  userID: string,
+) {
+  const [transactionRow] = await db
+    .select()
+    .from(transaction)
+    .where(
+      and(eq(transaction.id, transactionID), eq(transaction.userID, userID)),
+    );
+
+  if (!transactionRow) throw status(404, "Not Found");
+
+  await db.transaction(async (tx) => {
+    await tx.insert(transaction).values({
+      amount: transactionRow.amount,
+      balanceID: transactionRow.balanceID,
+      userID: transactionRow.userID,
+      categoryID: transactionRow.categoryID,
+      description: `REVERT: ${transactionRow.description}`,
+      type: "revert" as TransactionType.REVERT,
+    });
+
+    await tx.update(balance).set({
+      currentBalance: sql`${balance.currentBalance} + ${transactionRow.amount}`,
+    });
+  });
+
+  return;
+}
