c89f74fd70dce20709b8168ee43520b6d2a01af9
The-FerryMan1 <mercedroxy@gmail.com>
2025-11-22 15:10:18 +0800

    registered categories module

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 import { Elysia } from "elysia";
-import { openapi } from '@elysiajs/openapi'
+import { openapi } from "@elysiajs/openapi";
 import { betterAuth } from "./middleware/betterAuth";
 import cors from "@elysiajs/cors";
 import { FinancialAccountHandler } from "./modules/financial_account/financial_account.handler";
+import { CategoriesHandler } from "./modules/categories/categories.handler";
 
-
-const app = new Elysia({prefix: `/api/${Bun.env.API_VERSION as string}`})
-.use(
+const app = new Elysia({ prefix: `/api/${Bun.env.API_VERSION as string}` })
+  .use(
     cors({
       origin: ["http://localhost:3000", Bun.env.CLIENT_DOMAIN as string],
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
       credentials: true,
       allowedHeaders: ["Content-Type", "Authorization"],
-    }),
+    })
   )
   .use(openapi())
   .use(FinancialAccountHandler)
-  .listen(Bun.env.PORT as string || 3000)
+  .use(CategoriesHandler)
+  .listen((Bun.env.PORT as string) || 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
